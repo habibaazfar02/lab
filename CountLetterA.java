927beabffb243e7bdc3ebfb6823a824a3f5bc4e7
Habiba-Azfar <habibaazfar02@gmail.com>
2026-06-16 22:17:23 +0500

    Update CountLetterA.java

---
+++ CountLetterA.java
@@ -0,0 +1,32 @@
+//h
+public class CountLetterA {
+
+    public int totalA(String text) {
+
+        int total = 0;
+
+        for (char ch : text.toCharArray()) {
+
+            if (ch == 'A' || ch == 'a') {
+                total++;
+            }
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+        CountLetterA obj = new CountLetterA();
+
+        int result = obj.totalA("AASAAR");
+
+        System.out.println("Total A's = " + result);
+
+        if (result == 4) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+        }
+    }
+}
